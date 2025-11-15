24095f12a068034eac71f62ea1abee3db1237a8c
dnynesh9096 <dnyneshdevkar@gmail.com>
2025-11-15 11:32:19 +0530

    add user and  video model

--- src/utlis/ApiResponse.js
+++ src/utlis/ApiResponse.js
@@ -1,7 +1,7 @@
 class  ApiResponse{
     construtor(statusCode, data, message ="Success"){
         this.statusCode =statusCode
-        this.data =data
+        this.data = data
         this.message = message
         this.sucess = statusCode < 400
     }
